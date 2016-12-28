f05208badb486609e1260393d4d175b195e8dab7
maxlath <k@maxlath.eu>
2016-12-28 18:17:17 +0100

    WIP: systemd service

--- scripts/custom_setup.sh
+++ scripts/custom_setup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
+
+PROJECT_ROOT=$(pwd)
+
 mkdir ./data ./db
 cd ./data
 git init
 git remote add origin add git@github.com:inventaire/inventaire-wiki.git
 # If it is the first time, `git push origin master` to init
 # the remote master branch
+
+cd $PROJECT_ROOT
+./scripts/setup_systemd_service.sh
---
+++ scripts/setup_systemd_service.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+PROJECT_ROOT=$(pwd)
+NODE_VERSION=$(node -v)
+HOME_FOLDER=~
+
+cat $PROJECT_ROOT/etc/jingo.original.service |
+ sed "s@PROJECT_ROOT@$PROJECT_ROOT@g" |
+ sed "s@NODE_VERSION@$NODE_VERSION@g" |
+ sed "s@HOME_FOLDER@$HOME_FOLDER@g" |
+ sed "s@USERNAME@$USERNAME@g" > $PROJECT_ROOT/etc/jingo.service
+
+# Copy instead of simply linking as some system fail to find the symbolic link target
+sudo cp $PROJECT_ROOT/etc/jingo.service /etc/systemd/system/
+
+# setup jingo systemd service
+sudo systemctl daemon-reload
+sudo systemctl enable jingo.service
+sudo systemctl start jingo.service
