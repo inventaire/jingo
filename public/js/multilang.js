394541531f81b8760a12210b4033763117cca828
maxlath <k@maxlath.eu>
2016-12-30 17:51:29 +0100

    added more languages to langMap

--- public/js/multilang.js
+++ public/js/multilang.js
@@ -13,10 +13,20 @@
 // Avoid leaking variables to the global scope
 !(function () {
 
+  // Those are the active languages in inventaire
+  // https://github.com/inventaire/inventaire-client/blob/943fe11140ade7a0f966aebbc9097d9d974e3cf8/app/lib/languages_data.coffee
   var langMap = {
+    da: 'Dansk',
+    de: 'Deutsch',
     en: 'English',
+    es: 'Español',
     fr: 'Français',
-    es: 'Español'
+    it: 'Italiano',
+    ja: '日本語',
+    no: 'Norsk',
+    pl: 'Polski',
+    pt: 'Português',
+    sv: 'Svenska'
   }
 
   var $content = $('#content')
