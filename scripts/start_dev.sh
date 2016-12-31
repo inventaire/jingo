32760fc4c8dcc27a70963db47f31b5e4cb5edfa9
maxlath <k@maxlath.eu>
2016-12-31 16:56:05 +0100

    dev mode: watch app.js too

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 nodemon ./jingo -c config.yaml &
-onchange-mini ./public/css/style.scss ./public/js/multilang.js -- npm run build dev-mode
+onchange-mini ./public/css/style.scss ./public/js/multilang.js ./public/js/app.js -- npm run build dev-mode
