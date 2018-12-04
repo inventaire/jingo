ce9db3b1a43457f4f27dfbc4fce1c7e9187636af
maxlath <k@maxlath.eu>
2018-12-04 12:41:15 -0800

    gitmech: make sure the ssh socket environment matches the ssh-agent

--- scripts/start_ssh_agent.sh
+++ scripts/start_ssh_agent.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-# Start the ssh-agent and setup the environment variables
-# to be available for ssh-add
+# Start the ssh-agent to make it available for ssh-add
 # see https://lowendbox.com/blog/generate-ssh-key-setup-ssh-agent/
-# Using '/tmp/ssh-agent' as a convention so that etc/jingo.base.service
-# knows were to find it: Environment=SSH_AUTH_SOCK=/tmp/ssh-agent
-eval $(ssh-agent -s -a /tmp/ssh-agent)
+eval $(ssh-agent -s)
 # Add the ssh key: will request the key password
 ssh-add ~/.ssh/id_rsa
+
+# The SSH_AUTH_SOCK will then be found by ./lib/set_ssh_socket_env.js
