e3e9e168160a8a8cb1b1a4eda264041f7782ae5c
maxlath <k@maxlath.eu>
2019-05-15 21:22:01 +0200

    start_dev script: restrict watcher to lib folder
    to avoid restarting the server when writing data files

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 nodemon ./jingo -c config.yaml &
-while true ; do inotifywait -r . -e modify && npm run build dev-mode && echo 'CHANGE'; done
+while true ; do inotifywait -r ./lib -e modify && npm run build dev-mode && echo 'CHANGE'; done
