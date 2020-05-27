ad4dafaa212cb04346856554934414cb9599128c
maxlath <k@maxlath.eu>
2020-05-27 19:40:33 +0200

    legacy redirection: use an absolute URL
    so that redirections to http://blog.inventaire.io/post/110540440753/mapping-resources-using-open-knowledge-starting
    don't end up on https://wiki.inventaire.io/wiki/Blog/post/110540440753/mapping-resources-using-open-knowledge-starting
    because http://blog.inventaire.io redirects to https://wiki.inventaire.io/wiki/Blog

--- routes/legacy.js
+++ routes/legacy.js
@@ -1,5 +1,6 @@
 var router = require('express').Router()
 
-router.get('/post/110540440753/*', (req, res) => res.redirect(301, '/wiki/mapping-resources-using-open-knowledge'))
+router.get('/post/110540440753/*', (req, res) => res.redirect(301, 'https://wiki.inventaire.io/wiki/mapping-resources-using-open-knowledge'))
+router.get('/wiki/Blog/post/110540440753/*', (req, res) => res.redirect(301, 'https://wiki.inventaire.io/wiki/mapping-resources-using-open-knowledge'))
 
 module.exports = router
